323649f2792cff19639ac38db391ebe16f007894
Jerome Leclanche <jerome@leclan.ch>
2017-09-19 15:05:26 +0300

    Add hearthsim_identity dependency

--- setup.py
+++ setup.py
@@ -3,4 +3,11 @@
 from setuptools import setup
 
 
-setup()
+setup(
+	# The following lets us find the hearthsim_identity egg.
+	# https://github.com/pypa/pip/issues/4187
+	dependency_links=[
+		"https://github.com/HearthSim/hearthsim-identity/archive/master.zip"
+		"#egg=hearthsim_identity-1.0.1"
+	]
+)
