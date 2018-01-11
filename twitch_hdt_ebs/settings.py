a645a643c0d5faa473ab73d134cc7a7828161408
Jerome Leclanche <jerome@leclan.ch>
2018-01-11 16:29:23 +0200

    Add default connect timeouts to cache access

--- twitch_hdt_ebs/settings.py
+++ twitch_hdt_ebs/settings.py
@@ -91,6 +91,8 @@ CACHES = {
 		"LOCATION": params.get("REDIS_CACHE_URL", "redis://127.0.0.1:6379/1"),
 		"OPTIONS": {
 			"CLIENT_CLASS": "django_redis.client.DefaultClient",
+			"SOCKET_CONNECT_TIMEOUT": 3,
+			"SOCKET_TIMEOUT": 3,
 		}
 	}
 }
