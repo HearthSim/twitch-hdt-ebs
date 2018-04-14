76d556a4ac73c70c9f36040f764a6d4100f41a4d
Benedict Etzel <benedict@hearthsim.net>
2018-04-14 17:15:32 +0200

    No longer track metrics for board_state messages

--- twitch_hdt_ebs/views.py
+++ twitch_hdt_ebs/views.py
@@ -149,13 +149,14 @@ class PubSubSendView(BaseTwitchAPIView):
 		}
 		resp = twitch_client.send_pubsub_message(self.request.twitch_user_id, pubsub_data)
 
-		write_point(
-			"pubsub_message",
-			{"count": 1},
-			channel_id=self.request.twitch_user_id,
-			status_code=resp.status_code,
-			message_type=serializer.validated_data["type"]
-		)
+		if serializer.validated_data["type"] != "board_state":
+			write_point(
+				"pubsub_message",
+				{"count": 1},
+				channel_id=self.request.twitch_user_id,
+				status_code=resp.status_code,
+				message_type=serializer.validated_data["type"]
+			)
 
 		return Response({
 			"status": resp.status_code,
