1fcb44edb1943b49b62c00d789118b0ee1c69d31
Benedict Etzel <benedict@hearthsim.net>
2020-04-10 14:06:33 +0200

    Drop an unused middleware

--- twitch_hdt_ebs/settings.py
+++ twitch_hdt_ebs/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
 	"django.middleware.common.CommonMiddleware",
 	"django.middleware.csrf.CsrfViewMiddleware",
 	"django.contrib.auth.middleware.AuthenticationMiddleware",
-	"django.contrib.messages.middleware.MessageMiddleware",
 	"django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
