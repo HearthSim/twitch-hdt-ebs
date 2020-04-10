c117194aa0cfd910a743e328074e8cf5038fa7a4
Benedict Etzel <benedict@hearthsim.net>
2020-04-10 15:00:03 +0200

    Add redirect to /

--- twitch_hdt_ebs/urls.py
+++ twitch_hdt_ebs/urls.py
@@ -1,15 +1,18 @@
 """
 URL Configuration
 """
-
-from django.conf.urls import url
+from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 
 urlpatterns = [
-	url(r"^config/$", views.SetConfigView.as_view(), name="ebs_config"),
-	url(r"^setup/$", views.ExtensionSetupView.as_view(), name="ebs_setup"),
-	url(r"^send/$", views.PubSubSendView.as_view(), name="ebs_pubsub_send"),
-	url(r"^ping/$", views.PingView.as_view()),
+	path("", RedirectView.as_view(
+		url="https://github.com/hearthsim/twitch-hdt-ebs", permanent=False
+	)),
+	path("config/", views.SetConfigView.as_view(), name="ebs_config"),
+	path("setup/", views.ExtensionSetupView.as_view(), name="ebs_setup"),
+	path("send/", views.PubSubSendView.as_view(), name="ebs_pubsub_send"),
+	path("ping/", views.PingView.as_view()),
 ]
